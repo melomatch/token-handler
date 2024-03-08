2627b9a4fa1dd8f7f7874fd7b377d02355606073
Vitaly Zorin <addefan@mail.ru>
2024-03-08 18:51:23 +0300

    feat: add userscript template

---
+++ token-handler.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name Melomatch token handler
+// @namespace melomatch
+// @version 0.0.0
+// @description An extension for processing the receipt of a token required to work with the Yandex Music API
+// @description:ru Расширение для обработки получения токена, необходимого для работы с Yandex Music API
+// @author addefan, amirdianov, dasha.ved93
+// @match https://music.yandex.ru/
+// @match https://melomatch.ru/
+// @match http://dev.melomatch.ru/
+// @icon https://storage.yandexcloud.net/melomatch/icon/icon.png
+// @grant none
+// ==/UserScript==
+
+(function() {
+    "use strict";
+    // Your code here...
+})();
