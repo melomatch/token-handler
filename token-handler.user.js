9477c64b1ed1ed7a8f9c674557a6cfaf3c859b85
Vitaly Zorin <addefan@mail.ru>
2024-03-16 21:53:43 +0300

    feat: handle yandex token and redirect to melomatch Refs: #21

--- token-handler.user.js
+++ token-handler.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Melomatch token handler
 // @namespace melomatch
-// @version 0.0.0
+// @version 0.1.0
 // @description An extension for processing the receipt of a token required to work with the Yandex Music API
 // @description:ru Расширение для обработки получения токена, необходимого для работы с Yandex Music API
 // @author addefan, amirdianov, dasha.ved93
@@ -10,9 +10,27 @@
 // @match http://dev.melomatch.ru/
 // @icon https://storage.yandexcloud.net/melomatch/icon/icon.png
 // @grant none
+// @run-at document-body
+// @updateURL https://github.com/melomatch/token-handler/raw/main/token-handler.user.js
+// @downloadURL https://github.com/melomatch/token-handler/raw/main/token-handler.user.js
 // ==/UserScript==
 
 (function() {
     "use strict";
-    // Your code here...
+    const SITE_HOST = "http://dev.melomatch.ru";
+
+    const handleYandexToken = () => {
+        const hash = new URLSearchParams(window.location.hash.substring(1));
+        const accessToken = hash.get("access_token");
+
+        if (accessToken) {
+            window.location.replace(`${SITE_HOST}/callback/yandex?access_token=${accessToken}`);
+        }
+    };
+
+    switch (window.location.host) {
+        case "music.yandex.ru":
+            handleYandexToken();
+            break;
+    }
 })();
